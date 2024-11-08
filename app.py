ce81d7ee5ac3088eeb96e18b77728a572ec5a5f4
stjonas <jonasstoegerer@yahoo.at>
2024-11-08 16:11:13 +0100

    added hello counter and bind mount for message.txt

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 import redis
 import os
 
@@ -13,17 +13,31 @@ file_path = "/data/message.txt"
 
 @app.route("/")
 def hello():
+    # Increment the counter
+    count = r.incr("hello_count")
+
     # Set a value in Redis
     r.set("message", "Welcome to the Docker Tutorial!")
 
     # Retrieve the value from Redis
     message = r.get("message").decode("utf-8")
 
-    # Write the message to a file
+    # Write the message and count to a file
     with open(file_path, "w") as file:
-        file.write(message)
+        file.write(f"{message}\nHello has been called {count} times.")
 
-    return message
+    return f"{message}\nHello has been called {count} times."
+
+
+@app.route("/read-file")
+def read_file():
+    try:
+        if os.path.exists(file_path):
+            with open(file_path, "r") as file:
+                content = file.read()
+            return jsonify({"file_content": content})
+    except Exception as e:
+        return jsonify({"error": "Internal Server Error"}), 500
 
 
 if __name__ == "__main__":
